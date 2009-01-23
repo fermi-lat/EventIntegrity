5c9d66f79c31ec74fa4b92ac0ca63dace1901efd
cvs2git <>
2009-01-23 00:07:00 +0000

    This commit was manufactured by cvs2svn to create tag 'EventIntegrity-00-06-00'.
    Sprout from master 2009-01-23 00:06:59 UTC ecephas 'Check in for GlastRelease-15-49-00'
    Delete:
        .cvsignore
        ChangeLog
        cmt/.cvsignore
        cmt/requirements
        doc/release.notes
        src/.cvsignore
        src/Dll/EventIntegrity_dll.cxx
        src/Dll/EventIntegrity_load.cxx
        src/EventIntegrityAlg.cxx
        src/mainpage.h
        src/test/jobOptions.txt
        src/test/test_EventIntegrity.cxx

--- src/Dll/EventIntegrity_dll.cxx
+++
@@ -1,8 +0,0 @@
-/** @file EventIntegrity_dll.cxx
-
-  $Header: /nfs/slac/g/glast/ground/cvs/Trigger/src/Trigger_dll.cxx,v 1.5 2004/07/25 21:13:59 burnett Exp $
-*/
-
-#include "GaudiKernel/LoadFactoryEntries.h"
-LOAD_FACTORY_ENTRIES(EventIntegrity)
-
